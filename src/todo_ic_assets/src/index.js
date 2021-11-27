fb97ee12f3d485c0e5e500dffec542cbee6c81bb
bioerrorlog <n.iijima.0x01@gmail.com>
2021-11-27 21:41:42 +0900

    Initial commit.

---
+++ src/todo_ic_assets/src/index.js
@@ -0,0 +1,19 @@
+import { todo_ic } from "../../declarations/todo_ic";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await todo_ic.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
