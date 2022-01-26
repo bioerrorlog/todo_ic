28fd2697edcbd919d4e1dada93e3ef9870d279be
bioerrorlog <n.iijima.0x01@gmail.com>
2022-01-26 23:06:09 +0900

    delete old style components

--- src/todo_ic_assets/src/App.jsx
+++ src/todo_ic_assets/src/App.jsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, Box } from "@chakra-ui/react"
 import React, { useState, useEffect } from 'react';
 import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 import styled from 'styled-components'
@@ -154,52 +154,48 @@ const App = () => {
   return (
     <>
       <ChakraProvider>
-        <div className='app'>
-          <div className="content">
-            <div style={{ margin: "30px" }}>
-              {connected ? `Connected to plug: ${principalId} to canister ${canisterId}`: (
-                <PlugConnect
-                  host={network}
-                  whitelist={whitelist}
-                  dark
-                  onConnectCallback={handleConnect}
-                />
-              )}
-            </div>
+        <Box m={30}>
+          {connected ? `Connected to plug: ${principalId} to canister ${canisterId}`: (
+            <PlugConnect
+              host={network}
+              whitelist={whitelist}
+              dark
+              onConnectCallback={handleConnect}
+            />
+          )}
+        </Box>
 
-            {/* Greet func for Debug */}
-            {/* <div style={{ margin: "30px" }}>
-              <input
-                id="name"
-                value={name}
-                onChange={(ev) => setName(ev.target.value)}
-              ></input>
-              <button onClick={doGreet}>Greet</button>
-            </div>
-            <div>
-              Greet response: "
-              <span>{message}</span>"
-            </div>
-            <div style={{ margin: "30px" }}>
-            </div> */}
-
-            <DragDropContext onDragEnd={onDragEnd}>
-              <Droppable droppableId='all-columns' direction='horizontal' type='column'>
-                {(provided) => (
-                  <Container {...provided.droppableProps} ref={provided.innerRef}>
-                    {data.columnOrder.map((id, index) => {
-                      const column = data.columns[id]
-                      const tasks = column.taskIds.map(taskId => data.tasks[taskId])
-
-                      return <Column key={column.id} column={column} tasks={tasks} index={index} />
-                    })}
-                    {provided.placeholder}
-                  </Container>
-                )}
-              </Droppable>
-            </DragDropContext>
-          </div>
+        {/* Greet func for Debug */}
+        {/* <div style={{ margin: "30px" }}>
+          <input
+            id="name"
+            value={name}
+            onChange={(ev) => setName(ev.target.value)}
+          ></input>
+          <button onClick={doGreet}>Greet</button>
         </div>
+        <div>
+          Greet response: "
+          <span>{message}</span>"
+        </div>
+        <div style={{ margin: "30px" }}>
+        </div> */}
+
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId='all-columns' direction='horizontal' type='column'>
+            {(provided) => (
+              <Container {...provided.droppableProps} ref={provided.innerRef}>
+                {data.columnOrder.map((id, index) => {
+                  const column = data.columns[id]
+                  const tasks = column.taskIds.map(taskId => data.tasks[taskId])
+
+                  return <Column key={column.id} column={column} tasks={tasks} index={index} />
+                })}
+                {provided.placeholder}
+              </Container>
+            )}
+          </Droppable>
+        </DragDropContext>
       </ChakraProvider>
     </ >
   );
