cd4ca0e63572e21252bf8324f79534e8b71f9575
bioerrorlog <n.iijima.0x01@gmail.com>
2022-01-27 21:49:48 +0900

    apply React.StrictMode

--- src/todo_ic_assets/src/index.jsx
+++ src/todo_ic_assets/src/index.jsx
@@ -4,8 +4,10 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
-  <ChakraProvider>
-    <App/>
-  </ChakraProvider>, 
+  <React.StrictMode>
+    <ChakraProvider>
+      <App/>
+    </ChakraProvider>
+  </React.StrictMode>, 
   document.getElementById('root')
 );
