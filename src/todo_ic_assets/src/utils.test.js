3f7f4d67a506e03288159eccc936baae537fcc43
bioerrorlog <n.iijima.0x01@gmail.com>
2022-01-31 09:24:57 +0900

    align indents

--- src/todo_ic_assets/src/utils.test.js
+++ src/todo_ic_assets/src/utils.test.js
@@ -1,39 +1,39 @@
 import { convertArrayToObject } from './utils';
 
 describe('convertArrayToObject', () => {
-    test('convert with number item', () => {
-      const input = [
-        { id: 0, description: 'This is description 0.' },
-        { id: 1, description: 'This is description 1.' },
-        { id: 2, description: 'This is description 2.' },
-        { id: 3, description: 'This is description 3.' },
-      ];
-  
-      const output = {
-        '0': { id: 0, description: 'This is description 0.' },
-        '1': { id: 1, description: 'This is description 1.' },
-        '2': { id: 2, description: 'This is description 2.' },
-        '3': { id: 3, description: 'This is description 3.' },
-      };
+  test('convert with number item', () => {
+    const input = [
+      { id: 0, description: 'This is description 0.' },
+      { id: 1, description: 'This is description 1.' },
+      { id: 2, description: 'This is description 2.' },
+      { id: 3, description: 'This is description 3.' },
+    ];
 
-      expect(convertArrayToObject(input, 'id')).toStrictEqual(output);
-    });
+    const output = {
+      '0': { id: 0, description: 'This is description 0.' },
+      '1': { id: 1, description: 'This is description 1.' },
+      '2': { id: 2, description: 'This is description 2.' },
+      '3': { id: 3, description: 'This is description 3.' },
+    };
 
-    test('convert with string item', () => {
-      const input = [
-        { id: 'aaa', description: 'This is description 0.' },
-        { id: 'bbb', description: 'This is description 1.' },
-        { id: 'ccc', description: 'This is description 2.' },
-        { id: 'ddd', description: 'This is description 3.' },
-      ];
-  
-      const output = {
-        'aaa': { id: 'aaa', description: 'This is description 0.' },
-        'bbb': { id: 'bbb', description: 'This is description 1.' },
-        'ccc': { id: 'ccc', description: 'This is description 2.' },
-        'ddd': { id: 'ddd', description: 'This is description 3.' },
-      };
-
-      expect(convertArrayToObject(input, 'id')).toStrictEqual(output);
-    });
+    expect(convertArrayToObject(input, 'id')).toStrictEqual(output);
   });
+
+  test('convert with string item', () => {
+    const input = [
+      { id: 'aaa', description: 'This is description 0.' },
+      { id: 'bbb', description: 'This is description 1.' },
+      { id: 'ccc', description: 'This is description 2.' },
+      { id: 'ddd', description: 'This is description 3.' },
+    ];
+
+    const output = {
+      'aaa': { id: 'aaa', description: 'This is description 0.' },
+      'bbb': { id: 'bbb', description: 'This is description 1.' },
+      'ccc': { id: 'ccc', description: 'This is description 2.' },
+      'ddd': { id: 'ddd', description: 'This is description 3.' },
+    };
+
+    expect(convertArrayToObject(input, 'id')).toStrictEqual(output);
+  });
+});
