88de095c07eea2dba2932ccc2971a6abc88983d8
bioerrorlog <n.iijima.0x01@gmail.com>
2022-02-02 18:29:49 +0900

    rename column id

--- src/todo_ic_assets/src/dataset.jsx
+++ src/todo_ic_assets/src/dataset.jsx
@@ -3,23 +3,16 @@ export const taskDataset = {
     "task-1": { id: "task-1", content: "Content for task 1" },
     "task-2": { id: "task-2", content: "Content for task-2" },
     "task-3": { id: "task-3", content: "Content for task-3" },
-    "task-4": { id: "task-4", content: "Content for task-4" }
+    "task-4": { id: "task-4", content: "Content for task-4" },
   },
-  // columns: {
-  //   "column-1": { id: "column-1", title: "Todo", taskIds: ['task-1']},
-  //   "column-2": { id: "column-2", title: "In progress", taskIds: ['task-2', 'task-3'] },
-  //   "column-3": { id: "column-3", title: "Review", taskIds: [] },
-  //   "column-4": { id: "column-4", title: "Completed", taskIds: ["task-4"] }
-  // },
-  // columnOrder: ["column-1", "column-2", "column-3", "column-4"],
 }
 
 export const columnDataset = {
   columns: {
-    "column-1": { id: "column-1", title: "Todo", taskIds: ['task-1']},
-    "column-2": { id: "column-2", title: "In progress", taskIds: ['task-2', 'task-3'] },
-    "column-3": { id: "column-3", title: "Review", taskIds: [] },
-    "column-4": { id: "column-4", title: "Completed", taskIds: ["task-4"] },
+    "backlog": { id: "backlog", title: "Backlog", taskIds: ['task-1']},
+    "inProgress": { id: "inProgress", title: "In progress", taskIds: ['task-2', 'task-3'] },
+    "review": { id: "review", title: "Review", taskIds: [] },
+    "done": { id: "done", title: "Done", taskIds: ["task-4"] },
   },
-  columnOrder: ["column-1", "column-2", "column-3", "column-4"],
+  columnOrder: ["backlog", "inProgress", "review", "done"],
 }
