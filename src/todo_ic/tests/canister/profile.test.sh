762821c306a7d46282321f285e5f95340479944a
bioerrorlog <n.iijima.0x01@gmail.com>
2022-02-22 22:50:54 +0900

    add canister test case

--- src/todo_ic/tests/canister/profile.test.sh
+++ src/todo_ic/tests/canister/profile.test.sh
@@ -6,14 +6,18 @@ load "prelude.sh";
 call todo_ic.listProfiles();
 assert _ == vec {};
 
-// createProfile by Alice
+// Fail to updateProfile if profile does not exist
 identity Alice;
+call todo_ic.updateProfile(record {about="this is Alice updated"; name="Alice updated"});
+assert _ == variant { err = variant { notFound } };
+
+// createProfile by Alice
 call todo_ic.createProfile(record {about="this is Alice"; name="Alice"});
 assert _ == variant { ok };
 call todo_ic.listProfiles();
 assert _ ~= vec {record {about = "this is Alice"; name = "Alice";};};
 
-// Fail to createProfile with alreadyExists by Alice
+// Fail to createProfile if profile already exists
 call todo_ic.createProfile(record {about="this is Alice again"; name="Alice"});
 assert _ == variant { err = variant { alreadyExists } };
 
